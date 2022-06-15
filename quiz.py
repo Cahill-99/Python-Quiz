1c254a1a3fd068c78a303680d13ab97fe416efcd
Cahill-99 <ecahill001@gmail.com>
2022-06-15 17:16:58 -0230

    added quiz questions

--- quiz.py
+++ quiz.py
@@ -1,8 +1,28 @@
 print("welcome to my quiz game")
 
-playing = input("Do you want to play? ")
+playing = input("Do you want to start the car quiz? ")
 
 if playing != "yes":
     quit()
 
-print("Okay let's play :")
+print("Okay let's start :")
+
+answer = input("What does BOV stand for? ")
+if answer == "blow off valve":
+    print("Correct!")
+
+answer = input("What does ABS stand for? ")
+if answer == "anti lock braking system":
+    print("Correct!")
+
+answer = input("What does JDM stand for? ")
+if answer == "japanese domestic market":
+    print("Correct!")
+
+answer = input("What does AWD stand for? ")
+if answer == "all wheel drive":
+    print("Correct!")
+
+answer = input("What does AC stand for? ")
+if answer == "air conditioning":
+    print("Correct!")
